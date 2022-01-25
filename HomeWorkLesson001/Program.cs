20fab0eb617d80750b82063036f770a4e3c2af06
Viktorm <vm@viktorm.ru>
2022-01-25 20:54:26 +0300

    Homework for lesson 001 done

---
+++ HomeWorkLesson001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeWorkLesson001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"«Привет, {name}, сегодня {DateTime.Now}.");
+            Console.ReadLine();
+        }
+    }
+}
